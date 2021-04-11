cd7e8fb839edd284e80cca228c4df4d4a447c7ba
Mateus Devino <mateus@mdevino.com>
2021-04-10 23:29:43 -0300

    Adding coin column placement logic

--- Assets/Scripts/SrcGrid.cs
+++ Assets/Scripts/SrcGrid.cs
@@ -9,9 +9,14 @@ public class SrcGrid : MonoBehaviour
 
     private GameObject[] coins;
     private int coinsAdded;
-
     private int columns = 7;
     private int rows = 6;
+
+    private float cellWidth;
+    private float x0;
+    private float y0;
+    private float xf;
+    private float yf;
 
     private SpriteRenderer spriteRenderer;
 
@@ -24,6 +29,16 @@ public class SrcGrid : MonoBehaviour
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector3 size = spriteRenderer.bounds.size;
+        Vector3 center = spriteRenderer.bounds.center;
+        cellWidth = size.x / columns;
+        x0 = center.x - size.x / 2;
+        xf = center.x + size.x / 2;
+        y0 = center.y - size.y / 2;
+        yf = center.y + size.y / 2;
+
+        Debug.Log("x0=" + x0 + " xf=" + xf + " y0=" + y0 + " yf=" + yf);
+
         StartGame();
     }
 
@@ -34,10 +49,10 @@ public class SrcGrid : MonoBehaviour
         {
             // Translating pixels to game unities
             Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouse.z = 0f;
+            mouse.z = gameObject.transform.position.z;
             if (spriteRenderer.bounds.Contains(mouse))
             {
-                placeCoin(mouse, playerTurn);
+                placeCoin(mouse);
                 switchPlayers();
             }
         }
@@ -45,36 +60,30 @@ public class SrcGrid : MonoBehaviour
 
     private void switchPlayers()
     {
-        if (playerTurn == 1)
-        {
-            playerTurn = 2;
-        }
-        else
-        {
-            playerTurn = 1;
-        }
+        playerTurn = playerTurn == 1 ? 2 : 1;
     }
 
-    private void placeCoin(Vector3 mouse, int player)
+    private void placeCoin(Vector3 mouse)
     {
+        Vector3 newCoinPosition = new Vector3();
+        int column = GetColumn(mouse);
+        newCoinPosition.x = x0 + (column * cellWidth) + (cellWidth / 2);
+        newCoinPosition.y = yf - cellWidth / 2;
+        newCoinPosition.z = gameObject.transform.position.z;
+
         Vector3 objectPos = mouse;
         GameObject coin = playerTurn == 1 ? coin1 : coin2;
-        Instantiate(coin, objectPos, Quaternion.identity);
+        Instantiate(coin, newCoinPosition, Quaternion.identity);
         coins[coinsAdded] = coin;
         coinsAdded++;
 
-        Debug.Log(
-            "Player " + playerTurn + " has placed a coin!" + 
-            " Mouse: x=" + mouse.x + ", y=" + mouse.y + 
-            "Object: x=" + objectPos.x + ", y=" + objectPos.y
-            );
+        Debug.Log("Player " + playerTurn + " has placed a coin!  Mouse: x=" + mouse.x + ", y=" + mouse.y + "Object: x=" + newCoinPosition.x + ", y=" + newCoinPosition.y);
+
     }
 
     private void StartGame()
     {
-        if(coinsAdded > 0)
-            DeleteCoins();
-        coins = new GameObject[columns * rows];
+        DeleteCoins();
         // By default, int array values are set to 0 in C#
         grid = new int[columns, rows];
         playerTurn = 1;
@@ -82,10 +91,35 @@ public class SrcGrid : MonoBehaviour
 
     private void DeleteCoins()
     {
-        foreach(GameObject coin in coins)
+        if (coins != null && coins.Length != 0)
         {
-            Destroy(coin);
+            foreach (GameObject coin in coins)
+            {
+                Destroy(coin);
+            }
         }
+        coins = new GameObject[columns * rows];
         coinsAdded = 0;
+    }
+
+    private int GetColumn(Vector3 mouse)
+    {
+        int columnIndex = 0;
+        float columnStart = x0;
+        float columnEnd = x0 + cellWidth;
+        while(columnIndex < columns)
+        {
+            if (columnStart < mouse.x && mouse.x < columnEnd)
+            {
+                return columnIndex;
+            }
+            else
+            {
+                columnStart = columnEnd;
+                columnEnd = columnEnd + cellWidth;
+                columnIndex++;
+            }
+        }
+        return -1;
     }
 }
